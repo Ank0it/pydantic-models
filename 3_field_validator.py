6ab4ec470ec982b6b8be2196c820bd63e5ab58dd
Ankit Patel <131870832+Ank0it@users.noreply.github.com>
2026-02-08 19:23:30 +0530

    separate age validation logic and remove update_patient_details function

--- 3_field_validator.py
+++ 3_field_validator.py
@@ -5,7 +5,7 @@ class Patient(BaseModel):
     name: Annotated[str,Field(max_length=50,title='Name of the patient',
                        description='Give the name of patient in less than 50 chars',
                        examples=['Aknimi','Rakimini'])]
-    age:int=Field(gt=0,lt=120)
+    age:int
     linked_in_urls: AnyUrl
     email: EmailStr
     weight: Annotated[float,Field(gt=0,strict=True)]
@@ -32,6 +32,14 @@ class Patient(BaseModel):
     def transform_name(cls,value):
         return value.upper()
 
+    @field_validator('age',mode='before')
+    @classmethod
+    def check_age(cls,value):
+        if 0<value<100:
+            return value
+        else:
+            raise ValueError('Age should be between 0 to 100')        
+
 def insert_patient_details(patient:Patient):
     print(patient.name)
     print(patient.age)
@@ -41,19 +49,10 @@ def insert_patient_details(patient:Patient):
     print(patient.contact_details)
     print('inserted')
 
-def update_patient_details(patient:Patient):
-    print(patient.name)
-    print(patient.age)
-    print(patient.weight)
-    print(patient.married)
-    print(patient.allergies)
-    print(patient.contact_details)
-    print('updated')
-
 patient_info={'name':'yiia','age':'49','email':'kopoplu@icici.com',
         'linked_in_urls':'https://linkedin.com/yiia','weight':87.89,'married':False,
         'allergies':['pollen','dust'],'contact_details':{'phone':'9089090'}}    
     
-patient1=Patient(**patient_info)
+patient1=Patient(**patient_info)     # Validation -> type coersion        
 
 insert_patient_details(patient1)
